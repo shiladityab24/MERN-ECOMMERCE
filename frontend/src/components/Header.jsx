6be434c1a522b0e02b830c0c4627b29c412cd45f
shiladityab24 <shiladityab24@yahoo.com>
2023-09-23 23:46:16 +0530

    Added Bootstrap theme

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,12 +1,15 @@
 import { Navbar, Nav, Container} from 'react-bootstrap';
 import { FaShoppingCart, FaUser } from 'react-icons/fa';
-
+import logo from '../assets/logo.png';
 const Header = () => {
   return (
     <header>
         <Navbar bg="dark" variant="dark" expand="md" collapseOnSelect>
             <Container>
-                <Navbar.Brand href="/">Eshop</Navbar.Brand>
+                <Navbar.Brand href="/">
+                    <img src={logo} alt="logo" size={10}/>
+                    Eshop
+                </Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav"/>
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="ms-auto">
