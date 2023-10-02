a02e4d1a796619eea40d6e4c7f0579f895ae7191
shiladityab24 <shiladityab24@yahoo.com>
2023-10-02 06:48:14 +0530

    Custom error middleware errorMiddleware.js

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -5,6 +5,7 @@ import asyncHandler from '../middleware/asyncHandler.js';
 import Product from '../models/productModel.js';
 router.get('/',asyncHandler(async(req,res) => {
     const products = await Product.find({});
+    // throw new Error('Some Error');
     res.json(products);
 }));
 
@@ -13,7 +14,7 @@ router.get('/:id',asyncHandler(async(req,res)=> {
     if(product){
         return res.json(product);
     }
-    res.status(404).json({message: 'Product not found'});
+    res.status(404).json({message: 'Resource not found'});
 }));
 
 
--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 import connectDB from './config/db.js';
 import productRoutes from './routes/productRoutes.js';
+import { notFound,errorHandler } from './middleware/errorMiddleware.js';
 const port = process.env.PORT || 8000;
 
 connectDB();
@@ -17,6 +18,7 @@ app.get('/',(req,res)=> {
 
 app.use('/api/products',productRoutes);
 
-
+app.use(notFound);
+app.use(errorHandler);
 
 app.listen(port,() => console.log(`Server running on port ${port}`));
