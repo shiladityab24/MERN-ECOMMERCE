b94d9e7938e01bac0743fb87648a7759ef05baca
shiladityab24 <shiladityab24@yahoo.com>
2023-10-02 07:17:25 +0530

    Product controller updated by adding productController.js in the backend folder

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,22 +1,11 @@
 import express from 'express';
 // import products from '../data/products.js';
 const router = express.Router();
-import asyncHandler from '../middleware/asyncHandler.js';
-import Product from '../models/productModel.js';
-router.get('/',asyncHandler(async(req,res) => {
-    const products = await Product.find({});
-    // throw new Error('Some Error');
-    res.json(products);
-}));
-
-router.get('/:id',asyncHandler(async(req,res)=> {
-    const product = await Product.findById(req.params.id);
-    if(product){
-        return res.json(product);
-    }
-    res.status(404).json({message: 'Resource not found'});
-}));
+import {getProducts, getProductById} from '../controllers/productController.js';
 
 
 
+
+router.route('/').get(getProducts);
+router.route('/:id').get(getProductById);
 export default router;
