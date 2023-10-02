a6cb9095ee3212a109072cbf33f7711c0b91bfaf
shiladityab24 <shiladityab24@yahoo.com>
2023-10-02 14:54:57 +0530

    Added Redux store and slice API

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,7 @@ dotenv.config();
 import connectDB from './config/db.js';
 import productRoutes from './routes/productRoutes.js';
 import { notFound,errorHandler } from './middleware/errorMiddleware.js';
-const port = process.env.PORT || 8000;
+const port = process.env.PORT || 5000;
 
 connectDB();
 
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -6,6 +6,8 @@ import {
   Route,
   RouterProvider
 } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './store';
 import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
 // import 'bootstrap/dist/css/bootstrap.min.css';
@@ -27,7 +29,9 @@ const router = createBrowserRouter(
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <RouterProvider router={router}/>
+    <Provider store={store}>
+      <RouterProvider router={router}/>
+    </Provider>
   </React.StrictMode>
 );
 
